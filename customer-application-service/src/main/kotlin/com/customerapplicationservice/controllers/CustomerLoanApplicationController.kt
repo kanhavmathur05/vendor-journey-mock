194a479eb7049047ddd312d86020c315daa0b272
Kanhav Mathur <kanhavmathur05@gmail.com>
2021-08-18 00:29:28 +0530

    Implemented Loggers

--- customer-application-service/src/main/kotlin/com/customerapplicationservice/controllers/CustomerLoanApplicationController.kt
+++ customer-application-service/src/main/kotlin/com/customerapplicationservice/controllers/CustomerLoanApplicationController.kt
@@ -6,6 +6,7 @@ import com.customerapplicationservice.dto.OfferApiRequest
 import com.customerapplicationservice.exceptions.CustomerOfferException
 import com.customerapplicationservice.modal.CustomerLoanApplication
 import com.customerapplicationservice.services.CustomerLoanApplicationService
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -20,39 +21,49 @@ import reactor.core.publisher.Mono
 @RestController
 @CrossOrigin("*")
 class CustomerLoanApplicationController(private val customerLoanApplicationService: CustomerLoanApplicationService) {
-    private  val webClient = WebClient.create("http://localhost:9999")
+    private val webClient = WebClient.create("http://localhost:9999")
+
+    private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @PostMapping("/check-offer")
     fun getM(@RequestBody offerApiRequest: OfferApiRequest): Flux<CustomerOffer> {
-        print(offerApiRequest.contactNumber)
-        print(offerApiRequest.dob)
-      //  customerLoanApplicationService.checkOffer()
+        logger.info("Inside getOffer method")
+        logger.info("Offer Api request- " + offerApiRequest.contactNumber + " " + offerApiRequest.dob)
 
         return webClient.post().uri("/offerApi")
-            .body(BodyInserters.fromValue(offerApiRequest))
-            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .retrieve()
-            .onStatus(
-            HttpStatus::is4xxClientError){
-                Mono.error(CustomerOfferException())
-            }
-            .bodyToFlux(CustomerOffer::class.java)
+                .body(BodyInserters.fromValue(offerApiRequest))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .onStatus(
+                        HttpStatus::is4xxClientError) {
+                    Mono.error(CustomerOfferException())
+                }
+                .bodyToFlux(CustomerOffer::class.java)
     }
 
     @PostMapping("/save-application")
     fun saveCustomerApplication(@RequestBody customerLoanApplicationDetails: CustomerLoanApplication): ResponseEntity<Mono<CustomerLoanApplication>> {
-
-        return  ResponseEntity.ok().body(customerLoanApplicationService.saveCustomerApplication(customerLoanApplicationDetails))
+        logger.info("Inside CustomerLoanApplicationController-Save Application method")
+        logger.info(" " + customerLoanApplicationDetails.fullName + " " + customerLoanApplicationDetails.email + " " + customerLoanApplicationDetails.addressType + " " + customerLoanApplicationDetails.employmentType + " " + customerLoanApplicationDetails.gender + " " + customerLoanApplicationDetails.monthlySalary + " " + customerLoanApplicationDetails.city + " " + customerLoanApplicationDetails.residentialAddress)
+        val response: Mono<CustomerLoanApplication>? = customerLoanApplicationService.saveCustomerApplication(customerLoanApplicationDetails)
+        logger.info("Response:- $response")
+        return ResponseEntity.ok().body(response)
     }
 
     @PostMapping("/get-application-status")
-    fun getApplicationStatus(@RequestBody applicationRequest: ApplicationRequest): ResponseEntity<Mono<CustomerLoanApplication>> = ResponseEntity.ok().body(customerLoanApplicationService.getCustomerLoanApplicationStatus(applicationRequest.applicationId))
+    fun getApplicationStatus(@RequestBody applicationRequest: ApplicationRequest): ResponseEntity<Mono<CustomerLoanApplication>> {
+        logger.info("Inside getApplicationStatus method")
+        logger.info("applicationRequest data " + applicationRequest.applicationId)
+        val response: Mono<CustomerLoanApplication>? = customerLoanApplicationService.getCustomerLoanApplicationStatus(applicationRequest.applicationId)
+        logger.info("Response:- $response")
+        return ResponseEntity.ok().body(response)
+    }
 
     @KafkaListener(topics = ["t4"], groupId = "myGroupId")
-    fun getTopics(e: String) :Unit{
-        println("In pr $e")
+    fun getTopics(e: String) {
+        logger.info("In pr $e")
         customerLoanApplicationService.updateApplicationStatus(e);
-       // delayGenerateEvent(e)
+        // delayGenerateEvent(e)
     }
 
 }
--- customer-application-service/src/main/kotlin/com/customerapplicationservice/services/CustomerLoanApplicationService.kt
+++ customer-application-service/src/main/kotlin/com/customerapplicationservice/services/CustomerLoanApplicationService.kt
@@ -4,6 +4,8 @@ import com.customerapplicationservice.modal.CustomerLoanApplication
 import com.customerapplicationservice.repositories.ApplicationUpdateRepo
 //import com.customerapplicationservice.repositories.ApplicationUpdateRepo
 import com.customerapplicationservice.repositories.CustomerLoanApplicationRepo
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,50 +15,38 @@ var id = ""
 @Service
 class CustomerLoanApplicationService(private val customerLoanApplicationRepo: CustomerLoanApplicationRepo, private val customerLoanApplicationRepo2:ApplicationUpdateRepo)
 {
-
-
     @Autowired
     private val kafkaTemplate: KafkaTemplate<String, String>? = null
 
+    private val logger: Logger = LoggerFactory.getLogger(CustomerLoanApplicationService::class.java)
 
     fun saveCustomerApplication(customerApplicationDetails: CustomerLoanApplication): Mono<CustomerLoanApplication> {
+        logger.info("Inside Service - Save Customer Application method")
+        logger.info(" " + customerApplicationDetails.fullName + " " + customerApplicationDetails.email + " " + customerApplicationDetails.addressType + " " + customerApplicationDetails.employmentType + " " + customerApplicationDetails.gender + " " + customerApplicationDetails.monthlySalary + " " + customerApplicationDetails.city + " " + customerApplicationDetails.residentialAddress)
         customerApplicationDetails.applicationStatus = "InProgress"
-
-
         return customerLoanApplicationRepo.save(customerApplicationDetails).map { a ->
             delayGenerateEvent(a.id!!)
             a
         }
     }
 
-
     fun delayGenerateEvent(c: String) {
-        print(c)
+        logger.info("Inside delayGenerateEvent method with id: $c")
         //Thread.sleep(10000)
         kafkaTemplate?.send("t3", c)
     }
 
     fun getCustomerLoanApplicationStatus(id: String): Mono<CustomerLoanApplication> {
+        logger.info("Inside getCustomerLoanApplicationStatus with ID: $id")
         val obj: Mono<CustomerLoanApplication> = customerLoanApplicationRepo.findById(id)
         return customerLoanApplicationRepo.findById(id);
     }
 
     fun updateApplicationStatus(id: String): String {
         //customerLoanApplicationRepo.changePassword(id,"Approved")
+        logger.info("Inside updateApplicationStatus method with id: $id")
         print("In Update Application")
-
         print(customerLoanApplicationRepo2!!.updateStatus(id) )
-
-
-/*        var c: Mono<CustomerLoanApplication> = customerLoanApplicationRepo.findById(id)
-
-        c.map {
-                a -> a.applicationStatus="Approved"
-            a
-        }
-            .subscribe {a -> customerLoanApplicationRepo.save(a)
-        }
-*/
         return "done"
 
     }
